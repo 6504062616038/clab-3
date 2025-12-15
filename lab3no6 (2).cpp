fc19e23f20dd1b3c81a56b3e216eef7b5fda5e8c
6504062616038 <153298618+6504062616038@users.noreply.github.com>
2025-12-15 16:46:00 +0700

    Add files via upload

---
+++ lab3no6 (2).cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void) {
+	int score;
+	
+	scanf("%d",&score);
+	//A 85 B 75 C > 55
+	if(score == 0 || score > 100){
+		printf("error score");
+	}
+	else if (score >= 68) {
+		if(score >= 85)
+			printf("%d(A)",score);
+		else if(score >= 75)
+			printf("%d(B)",score);
+		else
+			printf("%d score u get B (C)",75-score);
+		}
+	else{
+		// D 55 F
+		if(score >= 55)
+			printf("%d(D)",score);
+		else
+			printf("%d(F)",score);
+		}
+
+}
+
